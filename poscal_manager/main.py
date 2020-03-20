d4c9853a1fd59e97bc1a7e2fbf874624ba9fb7cd
Duan Yutong <givoltage@gmail.com>
2020-03-20 17:58:44 -0400

    added calibration index table display and heatmaps for R1+R2, fitting residuals, and gear ratios for theta and phi axes

--- poscal_manager/main.py
+++ poscal_manager/main.py
@@ -3,55 +3,211 @@
 Created on Thu Mar 19 15:14:18 2020
 
 @author: Duan Yutong (dyt@physics.bu.edu)
+
+start server with the following command:
+
+bokeh serve poscal_manager --allow-websocket-origin=desi-2.kpno.noao.edu:5006
+
+view at: http://desi-2.kpno.noao.edu:5006/poscal_manager
 """
 
-from bokeh.layouts import row
+import poscal_manager as pm
+from bokeh.io import curdoc  # , output_file, save
+# from bokeh.layouts import row
 from bokeh.plotting import figure
+from bokeh.palettes import Magma256
 from bokeh.models import (
     LinearColorMapper, ColorBar, AdaptiveTicker, LabelSet)
-from bokeh.models import ColumnDataSource
+from bokeh.models import ColumnDataSource, Button, CheckboxButtonGroup
+from bokeh.models.widgets.markups import Div
 from bokeh.models.widgets.tables import (
-    DataTable, TableColumn, IntEditor
-)
-from bokeh.io import curdoc
+    DataTable, TableColumn, SelectEditor, IntEditor)
+from bokeh.layouts import layout
+import numpy as np
+np.seterr(all='raise')
 
 
-import os
-import pickle
-from glob import glob
-import posconstants as pc
+pcids = list(range(10))
+fpa_experts = ['Duan', 'Fanning', 'Fagrelius', 'Schubnell',
+               'Silber', 'Poppett']
+pcm = pm.PosCalManager()
+pi = pm.get_positioner_index()
+source = ColumnDataSource(data=pcm.table)
+source.selected.indices = [pcm.i_selected]  # ['1d']['indices'] = [i_selected]
+title = Div(text='''
+<font size="4">Positioner Calibrations</font> (some columns editable)''',
+            width=500)
+columns = [TableColumn(field='UTC', title='UIC', width=160),
+           TableColumn(field='expid', title='expid', width=50),
+           TableColumn(field='test name', title='test name', width=260),
+           TableColumn(field='dome', title='dome', width=50,
+                       editor=SelectEditor(options=['open', 'closed', '?'])),
+           TableColumn(field='zenith angle', title='zenith angle', width=69,
+                       editor=IntEditor()),
+           TableColumn(field='tracking', title='tracking', width=50,
+                       editor=SelectEditor(options=['on', 'off', '?'])),
+           TableColumn(field='PMTC', title='PMTC', width=50,
+                       editor=SelectEditor(options=['on', 'off', '?'])),
+           TableColumn(field='PCVF', title='PCVF', width=50,
+                       editor=SelectEditor(options=['on', 'off', '?'])),
+           TableColumn(field='dome louvers', title='dome louvers', width=75,
+                       editor=SelectEditor(options=['on', 'off', '?'])),
+           TableColumn(field='dome fans', title='dome fans', width=60,
+                       editor=SelectEditor(options=['on', 'off', '?'])),
+           TableColumn(field='B29 fan', title='B29 fan', width=50,
+                       editor=SelectEditor(options=['on', 'off', '?'])),
+           TableColumn(field='cage baffle', title='cage baffle', width=63,
+                       editor=SelectEditor(options=['on', 'off', '?'])),
+           TableColumn(field='barrel insulation', title='barrel insulation',
+                       width=85,
+                       editor=SelectEditor(options=['on', 'off', '?'])),
+           TableColumn(field='operator', title='operator',
+                       width=70,
+                       editor=SelectEditor(options=sorted(fpa_experts)+['?'])),
+           TableColumn(field='data path', title='data path', width=700)]
+table = DataTable(source=source, columns=columns, editable=True,
+                  sortable=False, reorderable=False, fit_columns=False,
+                  default_size=1300, height=min(27*pcm.len+30, 600),
+                  min_width=1300, sizing_mode='stretch_width')
+plot_bt = Button(label="Plot Selected Calibration", button_type='primary',
+                 width=300)
+ptls_bt_group = CheckboxButtonGroup(labels=[f'PC{i:02}' for i in pcids],
+                                    active=pcids, width=300)
 
-class PosCalManager:
 
-    table_filename = 'poscal_index.csv'
-
-    def __init__(self):
-        temp = os.path.join(pc.dirs['kpno'], '*/*/calibrationdata.pkl')
-        self.data_paths = [p for p in glob(temp) if
-                           'arc_calibration' in p or 'grid_calibration' in p]
-
-    def init_table(self):
-        # if table already exists, load table
-        if os.path.isfile(self.table_filename):
-            self.df = pd.read_csv(self.table_filename)
-            self.table = df.to_dict()  # dict with column names as keys
-            self.data_paths = [path for path in self.data_paths
-                               if path not in self.table['path']]
-        else:  # construct new table
-            self.table = d = {'UTC': [], 'expid': [],
-                              'test name': [], 'dome': [], 'path': []}
-        for path in self.data_paths:  # fill in records for all paths
-            with open(os.path.join(path), 'rb') as h:
-                data = pickle.load(h)
-            d['UTC'].append(data.t_i.isoformat())
-            d['expid'].append(data.expid)
-            d['test name'].append(data.test_name)
-            d['path'].append(path)
-        self.df = pd.DataFrame(self.table)
-        self.df.to_csv(self.table_filename)
+def change_selected_calibration(attr, old, new):
+    pcm.i_selected = new[0]
+    print('selection changed', attr, old, pcm.i_selected)
 
 
-if __name__ == '__main__':
-    pcm = PosCalManager()
-    print(pcm.df)
+def on_change_source_data(attr, old, new):
+    # old, new, source.data are all the same
+    print('Source changed, updating manager data and saving to disk')
+    pcm.table = new
+    pcm.table_dict_to_df()
 
+
+def change_ptls(attr, old, new):
+    pcids = new
+    print('Changing checked petals', old, pcids)
+
+
+names = {'R1R2_sum': 'R1+R2', 'residuals': 'RMS residuals',
+         'GEAR_CALIB_T': 'Gear ratio θ', 'GEAR_CALIB_P': 'Gear ratio φ'}
+units = {'R1R2_sum': ' / mm', 'residuals': ' / μm',
+         'GEAR_CALIB_T': '', 'GEAR_CALIB_P': ''}
+lims = {'R1R2_sum': (5.5, 6.5), 'residuals': (0, 20),
+        'GEAR_CALIB_T': (0.8, 1.2), 'GEAR_CALIB_P': (0.8, 1.2)}
+
+
+def plot_heatmap(data, col):
+    '''col is a column name in calibdf, which can be
+    R1R2_sum, residuals, gear_ratio_T, gear_ratio_P'''
+    name, unit, lim = names[col], units[col], lims[col]
+    calibdf = process_calibdf(data.calibdf['FIT'])
+    calibdf['residuals'] *= 1000
+    tooltips = ([('cursor obsXY', '($x, $y)')]
+                + [(col, '@'+col) for col in calibdf.columns
+                   if 'residuals_' not in col])
+    title = f'{name}, expid {data.expid}, {data.mode} calibration'
+    heatmap = figure(
+        title=title, tools='pan,wheel_zoom,reset,hover,save',
+        x_range=(-420, 420), y_range=(-420, 420), tooltips=tooltips,
+        aspect_scale=1, plot_width=450, plot_height=500)
+    heatmap.xaxis.axis_label = 'obsX / mm'
+    heatmap.yaxis.axis_label = 'obsY / mm'
+    heatmap.hover.show_arrow = True
+    # low = calibdf[quantity].min(skipna=True)
+    # high = calibdf[quantity].max(skipna=True)
+    color_mapper = LinearColorMapper(
+        palette=Magma256, low=lim[0], high=lim[1])
+    heatmap_source = ColumnDataSource(calibdf)
+    heatmap.circle(
+        x='obs_x', y='obs_y', source=heatmap_source, radius=5,
+        fill_color={'field': col, 'transform': color_mapper},
+        fill_alpha=0.7, line_color='white', line_width=1.8,
+        hover_line_color='black')
+    colorbar = ColorBar(
+        title=name+unit, color_mapper=color_mapper,
+        ticker=AdaptiveTicker(), orientation='horizontal',
+        padding=5, location=(0, 0), height=10, width=330)
+    heatmap.add_layout(colorbar, place='above')  # above
+    return heatmap, heatmap_source
+
+
+def plot_histogram(data, quantity):
+    bottom = 0.1  # log cannot properly handle bottom = 0, set it above zero
+    hist = figure(
+        title=f'{quantity} distribution, {len(pcid)} petals',
+        y_axis_type='log', plot_width=450, plot_height=250)
+    # fields = [quantity] if 
+    # for device_type, color in zip(['pos', 'fid'], ['royalblue', 'orangered']):
+    #     p.quad(top=f'top_{petal_loc}_{device_type}', bottom=bottom,
+    #            left=f'left_{petal_loc}_{device_type}',
+    #            right=f'right_{petal_loc}_{device_type}',
+    #            source=source_hist, legend=device_type,
+    #            fill_color=color, line_color="white", alpha=0.5)
+    # p.y_range.start = bottom
+    # p.legend.location = "bottom_right"
+    # p.xaxis.axis_label = 'Temp / °C'
+    # p.yaxis.axis_label = 'Device Count'
+    # # add indicator labels only once
+    # labels = LabelSet(x='x', y='y', text='status_field', source=source_status,
+    #                   x_offset=10, render_mode='css', text_baseline='middle',
+    #                   text_font_size='11pt')
+    # p.circle(x='x', y='y', radius=0.4, alpha=1, line_alpha=0,
+    #          fill_color=f'color_{petal_loc}', source=source_status)
+    # p.add_layout(labels)
+    # return p
+
+
+def process_calibdf(calibdf):
+    calibdf = calibdf.join(pi)
+    calibdf['R1R2_sum'] = calibdf['LENGTH_R1'] + calibdf['LENGTH_R2']
+    if 'residuals' not in calibdf.columns:
+        npts = calibdf['residuals_T'][0].size + calibdf['residuals_P'][0].size
+        calibdf['residuals'] = np.sqrt(
+            ((calibdf['residuals_T']**2).apply(np.nansum)
+             + (calibdf['residuals_P']**2).apply(np.nansum))/npts)
+    return calibdf
+
+
+def update_plots():
+    print('current selected', pcm.i_selected)
+    path = pcm.df.loc[pcm.i_selected, 'data path']
+    data = pcm.read_pickle(path)
+    calibdf = process_calibdf(data.calibdf['FIT'])
+    print('Updating plots for', pcm.i_selected, path, pcids)
+    hm_r2r2_src.data = calibdf
+    hm_r1r2.title.text = f'R1+R2, expid {data.expid}, {data.mode} calibration'
+    hm_res_src.data = calibdf
+    hm_res.title.text = (
+        f'RMS residuals, expid {data.expid}, {data.mode} calibration')
+    if 'arc' in data.mode:
+        print('Updating gear ratio plots for arc calibration')
+        hm_grt_src.data = calibdf
+        hm_grt.title.text = (
+            f'Gear ratio θ, expid {data.expid}, {data.mode} calibration')
+        hm_grp_src.data = calibdf
+        hm_grp.title.text = (
+            f'Gear ratio φ, expid {data.expid}, {data.mode} calibration')
+
+
+path = pcm.df.loc[pcm.i_selected, 'data path']
+print('Plotting calibration for', pcm.i_selected, path, pcids)
+hm_r1r2, hm_r2r2_src = plot_heatmap(pcm.read_pickle(path), 'R1R2_sum')
+hm_res, hm_res_src = plot_heatmap(pcm.read_pickle(path), 'residuals')
+hm_grt, hm_grt_src = plot_heatmap(pcm.read_pickle(path), 'GEAR_CALIB_T')
+hm_grp, hm_grp_src = plot_heatmap(pcm.read_pickle(path), 'GEAR_CALIB_P')
+source.on_change('data', on_change_source_data)
+source.selected.on_change('indices', change_selected_calibration)
+plot_bt.on_click(update_plots)
+ptls_bt_group.on_change('active', change_ptls)
+layout = layout([[title],
+                 [table],
+                 [plot_bt, ptls_bt_group],
+                 [hm_r1r2, hm_res, hm_grt, hm_grp]])
+curdoc().title = 'DESI Positioner Calibration Manager'
+curdoc().add_root(layout)
+# output_file('main.html')
+# save(table)
---
+++ poscal_manager/poscal_manager.py
@@ -0,0 +1,175 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 19 16:59:03 2020
+
+@author: Duan Yutong (dyt@physics.bu.edu)
+
+
+Description of environmental conditions that are recorded in table
+
+* Primary Mirror Thermal Control (PMTC):
+    tl;dr:
+        * only during the day with dome closed when telescope is not in use
+        * mirror covers should be closed but could be open
+    This draws dome air in through a filter (you can see the large hose on
+    the SE side just under the railing on the C floor) into a heat
+    exchanger located behind the south journal which valves building
+    glycol to cool the air depending on the primary temperature setting
+    and dew point.  The air is then blown up through the East elliptical
+    tube into the space between the primary mirror and mirror covers.
+    This is used only during the day when the telescope is not being used.
+    One can hear the vibration from the large blower when it is on.
+
+* Primary Cell Vent Fans (PCVF):
+    tl;dr:
+        * night time dome closed only
+        * mirror covers should be open
+    These are the four squirrel cage fans on the center box section of the
+    telescope at the NE, SE, SW, and NW locations.  These are controlled
+    by the OA and are used to pull air from the space in front of the
+    primary around the periphery of the primary mirror and out into the
+    dome. The purpose of these is to avoid stagnant air cells which can
+    form in the closed space within the center section in front of the
+    primary and improve the image quality. As noted below, imaging
+    experiments have not suggested any degradation of the image quality
+    (and things should actually be better), but as far as I know, no high
+    frequency measurements have ever been carried out. Whether these can
+    induce vibration of the FVC is an open question. As noted, the OA can
+    turn these on or off as desired.  They are used at night with the
+    mirror covers open; the PMTC is always turned off when observing.
+
+* Dome Louvers:
+    While not properly fans, these are opened to flush air
+    through the dome to minimize temperature differences between the telescope
+    structure and the ambient air.  Depending on the wind speed and direction,
+    it may be possible to set up resonances which could vibrate the telescope
+    structure; this could be tested by opening/closing the louvers.
+
+* Dome Fans:
+    There are two large fans on the SSW side of the dome on the
+    M floor level.  Prior to the installation of the louvers, this provided
+    the only method of flushing air through the dome, and they were effectively
+    useless in that respect (we carried out a test with neutral buoyancy
+    balloons which demonstrated that turning these noisy fans on and off made
+    no difference to the air flow in the dome).  I don't know if they are used
+    anymore, but they do generate a bit of noise which could couple into
+    telescope vibrations.
+
+* B29 Fan:
+    This may or may not be re-installed. This is a huge fan which
+    sits on the M floor to the East of the hatch and, as its name suggests, is
+    extremely noisy.  It generates a vertical flow within the closed dome
+    during the day to avoid thermal stratification of the air and is supposed
+    to facilitate flushing of the dome air once the dome and louvers are opened
+    for nighttime observing.  The noise makes any work within the dome during
+    the day impossible, and this has typically been turned off when any daytime
+    work is going on or the mirror covers are open (because of dust that could
+    be stirred up).
+
+"""
+
+import sys
+import os
+import pickle
+from glob import glob
+from datetime import timezone
+import numpy as np
+import pandas as pd
+from DOSlib.positioner_index import PositionerIndex
+sys.path.append('/data/focalplane/pecs/')
+os.environ['POSITIONER_LOGS_PATH'] = '/data/focalplane/logs'
+os.environ['FP_SETTINGS_PATH'] = '/data/focalplane/fp_settings'
+import posconstants as pc
+from petaltransforms import PetalTransforms
+
+
+def get_positioner_index():  # get obsXY data for each device
+    pi = PositionerIndex()
+    pi_df = pd.DataFrame(pi.data).set_index('DEVICE_ID')
+    pi_df.columns = pi_df.columns.str.lower()
+    pi_df.insert(0, 'device_id', pi_df.index)
+    pi_df['obs_x'], pi_df['obs_y'] = np.nan, np.nan
+    ptlXYZ_df = pd.read_csv(os.path.join(
+            os.getenv('PLATE_CONTROL_DIR',
+                      '/software/products/plate_control-trunk'),
+            'petal', 'positioner_locations_0530v14.csv'),
+        usecols=['device_location_id', 'X', 'Y', 'Z'],
+        index_col='device_location_id')
+    ptlXYZ_df.index.rename('device_loc', inplace=True)
+    for pcid in range(10):
+        trans = PetalTransforms(gamma=np.pi/5*(pcid-3))
+        obsXY = trans.ptlXYZ_to_obsXYZ(ptlXYZ_df.T.values)[:2, :]
+        xy_df = pd.DataFrame(data=obsXY.T, index=ptlXYZ_df.index,
+                             columns=['obs_x', 'obs_y'])
+        xy_df['device_id'] = (pi_df[pi_df['petal_loc'] == pcid]
+                              .set_index('device_loc')['device_id'])
+        xy_df.set_index('device_id', inplace=True)
+        xy_df = xy_df[xy_df.index.notnull()]
+        pi_df.loc[xy_df.index, ['obs_x', 'obs_y']] = xy_df
+    return pi_df
+
+
+class PosCalManager:
+
+    table_path = os.path.join(os.path.abspath('.'), 'poscal_manager',
+                              'poscal_index.csv')
+
+    def __init__(self):
+        temp = os.path.join(pc.dirs['kpno'], '*/*/calibrationdata.pkl')
+        self.data_paths = [p for p in glob(temp) if
+                           'arc_calibration' in p or 'grid_calibration' in p]
+        self.data = {}
+        self.init_table()
+
+    def init_table(self):
+        cols = ['dome', 'zenith angle', 'tracking', 'PMTC', 'PCVF',
+                'dome louvers', 'dome fans', 'B29 fan',
+                'cage baffle', 'barrel insulation', 'operator']
+        if os.path.isfile(self.table_path):  # csv already exists, load df
+            self.df = pd.read_csv(self.table_path, index_col=0)
+            self.df_to_table_dict()
+            # filter out expids that have are already in the table
+            self.data_paths = [path for path in self.data_paths
+                               if path not in self.table['data path']]
+        else:  # no csv file exists, construct new table dict
+            self.table = d = {'UTC': [], 'expid': [], 'test name': [],
+                              'data path': []}
+            d.update({col: [] for col in cols})
+        if self.data_paths:  # fill table dict
+            for path in self.data_paths:  # fill in records for all paths
+                self.read_pickle(path)
+                data = self.data[path]
+                if data.posids[0][0] == 'D':
+                    continue  # filter out sim runs with Dxxxxx positioners
+                d['UTC'].append(data.t_i.astimezone(timezone.utc).strftime(
+                    pc.timestamp_format))
+                d['expid'].append(data.expid)
+                d['test name'].append(data.test_name)
+                d['data path'].append(path)
+                for col in cols:
+                    d[col].append('?')
+            self.table_dict_to_df()
+        self.len = len(self.df)
+        # select the latest arc calibration so that all plots are populated
+        self.i_selected = (
+            self.df[self.df['test name'].str.contains('arc')].index[-1])
+
+    def df_to_table_dict(self):
+        self.table = self.df.to_dict(orient='list')
+
+    def table_dict_to_df(self):
+        self.df = pd.DataFrame(self.table)
+        self.df.to_csv(self.table_path)
+
+    def read_pickle(self, path):
+        if path not in self.data:
+            with open(os.path.join(path), 'rb') as h:
+                self.data[path] = pickle.load(h)
+        return self.data[path]
+
+
+
+
+if __name__ == '__main__':
+    pcm = PosCalManager()
+    print(pcm.df)
